bb63ed0241f84d33a71bec6ff19cb1b929a13094
Punit Tyagi <Punittyagi.552@gmail.com>
2023-01-16 19:07:14 +0530

    New comp

---
+++ src/CompFirst.js
@@ -0,0 +1,8 @@
+function CompFirst() {
+    return (
+        <div>
+            <h1>This is First Component</h1>
+        </div>
+    );
+}
+export default CompFirst;
---
+++ src/CompSecond.js
@@ -0,0 +1,8 @@
+function CompSecond() {
+    return (
+        <div>
+            <h1>This is Second Component</h1>
+        </div>
+    );
+}
+export default CompSecond;
